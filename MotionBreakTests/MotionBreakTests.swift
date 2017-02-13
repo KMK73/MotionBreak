57d0c30fb34c68e093c366e12749ddc44b066382
Kelsey <kk@simplesolutionsfs.com>
2017-02-13 16:47:00 -0500

    Initial Commit

---
+++ MotionBreakTests/MotionBreakTests.swift
@@ -0,0 +1,36 @@
+//
+//  MotionBreakTests.swift
+//  MotionBreakTests
+//
+//  Created by Simple Solutions on 2/13/17.
+//  Copyright © 2017 Kelsey Kjeldsen. All rights reserved.
+//
+
+import XCTest
+@testable import MotionBreak
+
+class MotionBreakTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
